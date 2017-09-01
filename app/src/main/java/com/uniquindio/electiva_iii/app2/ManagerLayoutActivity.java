badc7ba7e7d6e2cae6ae895f9fe60e775bb7e600
Angela Londono <l.angela05@gmail.com>
2017-08-31 22:54:33 -0500

    Config Layouts

---
+++ app/src/main/java/com/uniquindio/electiva_iii/app2/ManagerLayoutActivity.java
@@ -0,0 +1,13 @@
+package com.uniquindio.electiva_iii.app2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManagerLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layout_activity);
+    }
+}
