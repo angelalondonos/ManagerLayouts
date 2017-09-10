86a8729b9cb23ea1876f1c1e9c12e991919eced0
Angela Londono <l.angela05@gmail.com>
2017-09-09 19:09:13 -0500

    Relative, grid, table Layouts

--- app/src/main/java/com/uniquindio/electiva_iii/app2/ManagerLayoutActivity.java
+++ app/src/main/java/com/uniquindio/electiva_iii/app2/ManagerLayoutActivity.java
@@ -8,6 +8,6 @@ public class ManagerLayoutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_layout_activity);
+        setContentView(R.layout.grid_layout_activity);
     }
 }
